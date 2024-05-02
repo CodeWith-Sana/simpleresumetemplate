5afed30caa3bdd171e8c3731a226f63bce38a35b
Ziaullah Khan <anoshakhancs27@gmail.com>
2024-05-02 22:54:11 +0500

    resume_html_css_js

---
+++ questions3.js
@@ -0,0 +1,50 @@
+alert('Sana Abdus Salam')
+/*var a=2
+ var i =1
+ while(i<11){
+    console.log( a + "  *" + "  "+ i + "  =  " + a*i)
+    i+=1
+ }
+
+*/
+//three sub marks
+/*var i =1
+var subj = 0  
+var total = 0
+while(i<4){
+   subj = parseInt(prompt("Enter your 3 subject marks  "))
+   total = total + subj
+    console.log("Subject  " + i + " =    " + subj)
+ 
+  i+=1
+}
+console.log("total marks =  " + total)
+var percent = total /3
+console.log("percentage =  " + percent)
+
+// Determine grade based on percentage
+switch (true) {
+    case (percent >= 90):
+        console.log("Grade is A");
+        break;
+    case (percent >= 80):
+        console.log("Grade is B");
+        break;
+    case (percent >= 65):
+        console.log("Grade is C");
+        break;
+    case (percent >= 40):
+        console.log("Grade is D");
+        break;
+    default:
+        console.log("Grade is F");
+        break;
+}*/
+/*var age = parseInt(prompt("Enter your age please!   "))
+if(age<=18){
+    console.log("Cradle is running")
+}
+else 
+console.log("Overaged")
+*/
+
